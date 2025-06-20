dd6e743103c0eb0ad4b183eab3ed34ddbc6c8425
Märten Mändla <marten.mandla@gmail.com>
2025-06-20 16:21:03 +0300

    Provider config first

---
+++ terraform.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket = "marten-tfstate"
+    key = "stock-lambda/terraform.tfstate"
+    region = "eu-north-1"
+    profile = "sec"
+  }
+
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = ">= 5.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = var.region
+  profile = "prod"
+}
